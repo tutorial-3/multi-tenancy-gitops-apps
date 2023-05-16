bbfe0b9d5959d53a59657b4cb09840d6596b9510
davidemoretti <davide.moretti@ibm.com>
2023-05-16 12:02:51 +0200

    changing name: from ace-integration-server-new to ace-integration-server

--- ace/environments/ci/pipelines/run-ace-is-pipeline.sh
+++ ace/environments/ci/pipelines/run-ace-is-pipeline.sh
@@ -9,7 +9,7 @@ tkn -n ci pipeline start \
   --param is-infra-repo-url=git@github.com:${GIT_ORG}/ace-infra.git \
   --param git-ops-repo-url=git@github.com:${GIT_ORG}/multi-tenancy-gitops-apps.git \
   --param gitops-apps-repo-full-name=${GIT_ORG}/multi-tenancy-gitops-apps \
-  --workspace name=shared-workspace,claimName=ace-integration-server-new \
+  --workspace name=shared-workspace,claimName=ace-integration-server \
   --workspace name=ace-infra-repo-secret,secret=ace-infra-at-github \
   --workspace name=gitops-repo-secret,secret=multi-tenancy-gitops-apps-at-github \
   --workspace name=ace-source-repo-secret,secret=${SOURCE_REPO}-at-github
